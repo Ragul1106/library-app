1505f7daa47fd1c23825711e2cb7455cbcff8e23
Ragul116 <ragulranjith1106@gmail.com>
2025-09-08 20:41:42 +0530

    add allowed hosts

--- library_project/settings.py
+++ library_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u#5eaq-(k!nnbxws$g+8k&e)fq9!k%(alqx-r@_2w-@nx=53+@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
